f8ef4c6bf9d494e9007bb7ef72691f0d9d33e6e6
RONALD OMONDI OMOLLO <collincesronald@gmail.com>
2025-07-20 04:01:35 +0300

    python Calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Welcome to the fun Calculator
+# Ask the user to input the first mumber
+num1 = float(input( "Enter the first number:"))
+# Ask the user to input the second number
+num2 = float (input ( "Enter the second number:"))
+# Time to do the Mathematics now welcome.
+# Add the two numbers
+sum_result = num1 + num2 
+# Subtract the two numbers 
+difference_result = num1 + num2 
+# Multiply th two numbers
+product_result = num1 * num2 
+# Divide the two numbers
+quotient_result = num1 / num2 
+# Show the user the result of our software product 
+print(f"Results of your two numbers:")
+print(f"Sum: {sum_result}") # Addition
+print(f"Difference: {difference_result}") # Subtraction
+print(f"Product: {product_result}") #Multiplication
+print(f"Quotient: {quotient_result}")
+# And you have just made a simple calculator.
